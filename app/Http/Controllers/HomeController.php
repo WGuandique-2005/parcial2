cbd9c8d24220fd3ad721cfbb0b55fc57b7ef1e82
WGuandique-2005 <wguandiquerivera07@gmail.com>
2025-05-09 11:32:37 -0600

    homeController and basic routes of the web

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+    public function signup()
+    {
+        return view('signup');
+    }
+}
